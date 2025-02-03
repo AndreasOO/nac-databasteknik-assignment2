e9f30586e243a9b916bf72d8d7127371b3c8eeca
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-02-03 09:58:15 +0100

    Added product entity

--- src/Model/Entity/Product/Product.java
+++ src/Model/Entity/Product/Product.java
@@ -1,5 +1,51 @@
 package Model.Entity.Product;
 
+import Model.Entity.Brand.Brand;
+import Model.Entity.Category.Category;
+import Model.Entity.ProductType.ProductType;
+
+import java.util.List;
+
 public class Product {
+    private final int id;
+    private final String name;
+    private final int price;
+    private final Brand brand;
+    private final ProductType productType;
+    private final List<Category> categories;
+
+    public Product(int id, String name, int price, Brand brand, ProductType productType, List<Category> categories) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.brand = brand;
+        this.productType = productType;
+        this.categories = categories;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public ProductType getProductType() {
+        return productType;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
     //TODO FINISH WHEN OTHER ENTITY CLASSES FOR BRAND, CATEGORY AND PRODUCT TYPE ARE DONE
 }
