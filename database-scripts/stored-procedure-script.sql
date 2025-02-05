002765f76456cb7f903f01f5e9047be03c4f3fec
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-02-05 13:14:36 +0100

    Added comments and changed quantity datatype from INT to UNSIGNED INT in order to trigger sqlerror when decremented to negative value.

--- database-scripts/stored-procedure-script.sql
+++ database-scripts/stored-procedure-script.sql
@@ -7,13 +7,19 @@ BEGIN
     DECLARE EXIT HANDLER FOR 1062
 		BEGIN 
             ROLLBACK;
-            resignal set message_text = 'An order with that id aldready exists, rolling back';
+            resignal set message_text = 'A completed order with that id aldready exists, rolling back';
         END;
         
     DECLARE EXIT HANDLER FOR 1264
 		BEGIN 
             ROLLBACK;
             resignal set message_text = 'The shop item is out of stock';
+        END;
+        
+    DECLARE EXIT HANDLER FOR 1452
+		BEGIN 
+            ROLLBACK;
+            resignal set message_text = 'There is no active order what that id';
         END;
         
 	DECLARE EXIT HANDLER FOR SQLEXCEPTION
@@ -26,17 +32,20 @@ BEGIN
     START TRANSACTION;
         IF EXISTS (SELECT `shop_db`.`orders`.`id` FROM `shop_db`.`orders` WHERE `shop_db`.`orders`.`customer_id`= `in_customer_id` AND `shop_db`.`orders`.`order_active` = 1) THEN
 			
+            
             SELECT `shop_db`.`orders`.`id` INTO `customerActiveOrderId` FROM `shop_db`.`orders` WHERE `shop_db`.`orders`.`customer_id`= `in_customer_id` AND `shop_db`.`orders`.`order_active` = 1;
             select `customerActiveOrderId`;
             IF (`in_order_id` IS NULL) THEN 
+            -- Active order exists and input order is null -> add input shop item to active order
             select '1:1 start';
             INSERT INTO `shop_db`.`order_items` (`shop_item_id`, `order_id`) VALUES (`in_shop_item_id`, `customerActiveOrderId`);
             select '1:1 end';
 			
-            
+            -- 
 			ELSEIF (`in_order_id` IS NOT NULL) THEN 
+            -- Active order exists and input order is specified -> add input shop item to active order with corresponding input order id -> if mismatch throw error 1452
             select '1:2 start';
-            INSERT INTO `shop_db`.`order_items` (`shop_item_id`, `order_id`) VALUES (`in_shop_item_id`, `customerActiveOrderId`);
+            INSERT INTO `shop_db`.`order_items` (`shop_item_id`, `order_id`) VALUES (`in_shop_item_id`, `in_order_id`);
             select '1:2 end';
 			END IF;
             
@@ -52,6 +61,8 @@ BEGIN
 		
         
 			ELSEIF (`in_order_id` IS NOT NULL) THEN 
+            -- Active order does not exists and input order is specified -> create new active order with input order id -> add shop item to active order 
+            -- WARNING: WILL SET NEW AUTO_INCREMENT START VALUE STARTING FROM in_order_id VALUE
 				select '2:2 start';
 				INSERT INTO `shop_db`.`orders` (`id`,`customer_id`, `order_active`) VALUES (`in_order_id`,`in_customer_id`, 1);
 				INSERT INTO `shop_db`.`order_items` (`shop_item_id`, `order_id`) VALUES (`in_shop_item_id`, `in_order_id`);
@@ -70,6 +81,6 @@ SET AUTOCOMMIT = 1;
 
 
 
-call addToCart(7,null,2);
-SELECT * from order_items WHERE order_items.order_id = 15;
+call addToCart(4,null,3);
+SELECT * from order_items WHERE order_items.order_id = 28;
 -- SELECT shop_db.`orders`.`id` FROM shop_db.`orders` WHERE shop_db.`orders`.`customer_id`= 1 AND shop_db.orders.`order_active` = 1;
